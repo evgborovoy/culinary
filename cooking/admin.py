9d14affc0643dc3d9ae9bd733830fbdb2b3656af
Evgeniy_Borovoy <borovoyevgen@gmail.com>
2025-08-01 21:47:05 +0400

    add dispaly_links in admin panels

--- cooking/admin.py
+++ cooking/admin.py
@@ -5,8 +5,10 @@ from django.contrib import admin
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ["pk", "title"]
+    list_display_links = ["pk", "title"]
 
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
     list_display = ["pk", "title", "created_at", "category"]
+    list_display_links = ["pk", "title"]
