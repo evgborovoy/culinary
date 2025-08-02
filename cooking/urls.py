c5f374134e768edc3c86eaf4182e1383ee19f8f3
Evgeniy_Borovoy <borovoyevgen@gmail.com>
2025-08-02 19:23:02 +0400

    add simple index page

--- cooking/urls.py
+++ cooking/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
+from .views import index
 
 app_name = "cooking"
 
 urlpatterns = [
-    path("/")
+    path("", index, name="index"),
 ]
--- cooking/views.py
+++ cooking/views.py
@@ -1,3 +1,11 @@
+from django.http import HttpRequest
 from django.shortcuts import render
 
+from .models import Category, Post
 
+def index(request: HttpRequest):
+    posts = Post.objects.all()
+    context = {
+        "posts": posts,
+    }
+    return render(request, "cooking/index.html", context=context)
